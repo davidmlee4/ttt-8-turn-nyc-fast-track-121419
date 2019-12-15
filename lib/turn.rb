22110ff8a1cc1873465a876c17ce7fb843a7584b
benevolent-trace-0172 <davidmlee4@gmail.com>
2019-12-15 05:15:18 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,7 +10,7 @@ def input_to_index(input)
   index = input.to_i - 1
 end
 
-def move(board, input_to_index, character = "X")
+def move(board, index, character = "X")
   board[index] = character
   return board
 end
