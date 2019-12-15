9c9f0092ea438dcbca8d735caea3f953e9c3aa1b
benevolent-trace-0172 <davidmlee4@gmail.com>
2019-12-15 05:22:08 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,7 +32,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   num = gets.chomp
-  index = num.to_i - 1
+  input_to_index(num)
   if valid_move?(board, index) == true
     move(board, index)
     display_board(board)
