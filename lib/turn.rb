376840617b49e64e406084ac1eca52b36651c88b
benevolent-trace-0172 <davidmlee4@gmail.com>
2019-12-15 04:39:19 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,23 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def move(board, input_to_index, character = "X")
+  board[index] = character
+  return board
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8)
+    return true
+  elsif !index.between?(0,8)
+    return false
+end
