1e009d130e7f656ab6a808533fccbf25ffe322b3
benevolent-trace-0172 <davidmlee4@gmail.com>
2019-12-15 05:38:12 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(input)
+  input.to_i - 1
+end
 
 def move(board, index, character = "X")
   board[index] = character
@@ -29,9 +32,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
-  def input_to_index(input)
-    index = input.to_i - 1
-  end
+  input_to_index(input)
   if valid_move?(board, index) == true
     move(board, index)
     display_board(board)
